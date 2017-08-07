661eabfd8980232b0e41d049ee17c5ffe588b294
GeorgeWei <weichenqi@gmail.com>
2017-08-07 16:17:40 +0800

    修复0.6

--- check_os.py
+++ check_os.py
@@ -50,7 +50,8 @@ print "The newest software sec update----> "+output
 content = open('/etc/passwd').read()
 reObj1 = re.compile('(\w+).*?/bin/bash')
 s = reObj1.findall(content)
-print "User's shell is /bin/bash with user: "+s[0:]
+for i in len(s):
+    print "User's shell is /bin/bash with user: "+s[i]
 
 
 (status, output) = commands.getstatusoutput('netstat -tunlp')
